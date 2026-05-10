f41056fcf3c8d88d62bae129e0a5b6e5bb993f2f
Katie Eastman <katieeastman@proton.me>
2026-05-10 14:03:03 -0600

    Delete src directory

--- src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-
-        <div class="mt-auto text-center w-100 bg-dark">
-            <p class="mb-0 text-light opacity-50">
-                This site is not affiliated with Nasa in any official capacity.
-            </p>
-        </div>
-    
-</template>
